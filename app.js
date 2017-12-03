839b9cf7d3fa93b1ab794114a8ba72c33a2fb630
AdityaHirapara <adityahirapara2016@gmail.com>
2017-12-03 19:58:25 +0530

    Mailing activity using node.js.

---
+++ app.js
@@ -0,0 +1,48 @@
+
+//modules
+var express	=	require('express');
+var app	=	express();
+var bodyParser	=	require('body-parser');
+var mailer = require('nodemailer');
+var pw = require('./password');
+
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/node_modules',express.static(__dirname + '/node_modules'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+	res.sendFile('mailform.html',{'root' : __dirname +''});
+});
+app.post('/mail',function(req,res){
+	var sender = mailer.createTransport({
+		service : 'gmail',
+		auth: {
+			user: 'adityahirapara2016@gmail.com',
+			pass: pw
+		}
+	});
+
+	var mail = {
+		from : req.body.email,
+		to : 'adityahirapara2016@gmail.com',
+		subject : req.body.question,
+		text : req.body.more
+	};
+
+	sender.sendMail(mail,function(error,msg){
+		if(error){
+			console.log(error);
+		}
+		else {
+			console.log('Success: ' + msg.response);
+		}
+	});
+	res.redirect('/');
+});
+
+//server connection
+app.listen(3000,function(){
+	console.log("server created!");
+});
